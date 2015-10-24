e17d2aea2f11934c4b73072f1d22267ef6d69b3d
Lau Llobet <lau.llobet@gmail.com>
2015-10-24 17:31:31 +0200

    *lock is now *passwd:

--- src/main/java/com/tubtale/otbackend/StaticStrings.java
+++ src/main/java/com/tubtale/otbackend/StaticStrings.java
@@ -5,7 +5,7 @@ public class StaticStrings {
     public static String KEYWORD_KEY = "*";
     public static String TWITTER_USER_KEY = "@";
     public static String BURY_KEYWORD = KEYWORD_KEY + "bury";
-    public static String LOCK_KEYWORD = KEYWORD_KEY + "lock";
+    public static String LOCK_KEYWORD = KEYWORD_KEY + "passwd:";
     public static String BURY_KEYWORD_FOR_REGEXP =  "\\" + BURY_KEYWORD;
     public static String LOCK_KEYWORD_FOR_REGEXP =  "\\" + LOCK_KEYWORD;
     public static Integer TWEET_LENGTH_WITH_LINK = 117;
